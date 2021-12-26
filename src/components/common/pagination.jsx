ac95843682f9aea21961bd721cb88594af176f04
swarajyj <yjswaraj@gmail.com>
2021-12-26 10:59:06 +0530

    filter implementaion

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -6,6 +6,7 @@ const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
   const pagesCount = Math.ceil(itemsCount / pageSize);
 
+  if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
 
   return (
