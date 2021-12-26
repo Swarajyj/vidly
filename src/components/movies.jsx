2f519b77eb5bd8dafbb5465ebbc2f31f46ed9669
swarajyj <yjswaraj@gmail.com>
2021-12-26 11:26:39 +0530

    filtering complete

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -16,7 +16,8 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() });
+    const genres = [{ name: "All" }, ...getGenres()];
+    this.setState({ movies: getMovies(), genres });
   }
 
   handleDelete = (movie) => {
@@ -38,7 +39,7 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    this.setState({ selectedGenre: genre });
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
   render() {
@@ -50,9 +51,10 @@ class Movies extends Component {
       movies: allMovies,
     } = this.state;
     if (count === 0) return <p>There are no movies in the database.</p>;
-    const filtered = selectedGenre
-      ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
-      : allMovies;
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+        : allMovies;
     const movies = paginate(filtered, currentPage, pageSize);
     return (
       <div className="row">
